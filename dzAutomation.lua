d67f88ef963b0a3464e9c87f3e8abece4cc842a7
Ron Peeters <ronnie.peeters@gmail.com>
2018-02-12 23:02:48 +0100

    Update dzAutomation.lua

--- dzAutomation.lua
+++ dzAutomation.lua
@@ -1,4 +1,4 @@
-local scriptVersion                 = '1.6.25'
+local scriptVersion                 = '1.6.38'
 
 
 return {
@@ -22,7 +22,7 @@ return {
     local ModeSelectorSecond            = 'Automation Secundary'	    
     local OperationMode           = domoticz.devices(ModeSelector).state
     local OperationSecondMode     = domoticz.devices(ModeSelectorSecond).state	
-	--domoticz.log('Change'..device.name..' level:'..device.level, domoticz.LOG_DEBUG)
+	domoticz.log('Change'..device.name..' level:'..device.level, domoticz.LOG_DEBUG)
 	
 	    if (device.isTimer) then
             if (domoticz.devices('Toon Scenes').level == 60 and domoticz.devices('Automation').level ~= 30) then
@@ -57,13 +57,18 @@ return {
 		-- Systeem status Vakantie
 			-- Automation met de hand op vakantie gezet. Dit kan niet. wordt terug gezet naar normaal.
 			domoticz.log('Domoticz set to vacation mode. This is not possible. Needs to be done through thermostat.', domoticz.LOG_ERROR)
-			device.switchSelector(10)
-		elseif (device.name == 'Automation' and device.level == 40 and OperationSecondMode ~= 'Aanwezigheid Override') then
+			domoticz.log('Domoticz switch '..device.name..' will be reverted to level '..tostring(device.lastlevel)..'.', domoticz.LOG_ERROR)
+			if device.lastlevel == nil then
+			    device.switchSelector(10)
+			else
+			    device.switchSelector(device.lastlevel).silent()
+			end
+		elseif (device.name == 'Automation' and device.level == 40 and OperationSecondMode ~= 'Aanwezigheid Override' and OperationSecondMode ~= 'Vakantie Kinderen en Aanwezigheid Override') then
 		-- Systeem status Weg
 			domoticz.log('Iedereen is zojuist vertrokken.', domoticz.LOG_DEBUG)
 			
 			--Verwarming handmatig op 16 graden zetten
-			domoticz.log('Changing thermostat program to temporary and temp to 16 degrees.', domoticz.LOG_INFO)
+			domoticz.log('Changing thermostat program to temporary and temp to 17.5 degrees.', domoticz.LOG_INFO)
 			domoticz.devices('Toon Auto Programma').switchSelector(30)
 			domoticz.devices('Toon Thermostaat').updateSetPoint(17.5)
 			time = os.date("*t")
