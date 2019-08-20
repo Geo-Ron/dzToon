567cad35e5cb16453c4a0da634fe67727d3f3a94
Ron Peeters <ronnie.peeters@gmail.com>
2019-08-20 21:40:24 +0200

    Update findmyToon.sh

--- findmyToon.sh
+++ findmyToon.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ #! /bin/sh
 ### BEGIN INFO
 # Description:       This script will find a device by its mac address and update the UV accordingly
 ### END INFO
@@ -6,7 +6,7 @@
 # Do NOT "set -e"
 
 #Crontab schedule
-*/5 * * * * /home/pi/findToon.sh
+#*/5 * * * * /home/pi/findToon.sh > /home/pi/findToon.log
 
 #OudeToon (staat nu op zolder)
 #toonMAC=6c:71:d9:3c:29:4e 
@@ -18,11 +18,15 @@ UVToonIP="UV_ToonIP"
 
 SUBNET=$(ip route show | awk '/24 dev/ {print $1}' | head -n1)
 
+printf "Scanning subnet $SUBNET...\n"
 #Scan entire subnet
 nmap $SUBNET -sn -sC 
 
 sleep 20
 #fetch arp table and insert IP into variable
 toonIP=$(arp -n | grep ${toonMAC} | awk '{print $1}')
+
+printf "For MAC $toonMAC I've found the ip $toonIP\n"
+printf "Setting variable using curl http://192.168.0.90:10080/json.htm?type=command&param=updateuservariable&vname=${UVToonIP}&vtype=2&vvalue=${toonIP}`n`"
 
 curl "http://192.168.0.90:10080/json.htm?type=command&param=updateuservariable&vname=${UVToonIP}&vtype=2&vvalue=${toonIP}"
